fe534ce6a3d4e59dbed1ea4b2a5f9fde962cdd88
jasparkatt <jasparkatt@yahoo.com>
2019-06-28 15:43:56 -0500

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,48 +1,32 @@
 var geojsonMarkerOptions = {
-    radius: 7,
-    fillColor: "#ff9900",
-    color: "#003366",
-    weight: 1,
+    radius: 5,
+    fillColor: "#3232ff",
+    color: "#ffff00",
+    weight: 0.5,
     opacity: 1,
     fillOpacity: 0.8
 };
 
 var layers = {
-    layer1: {
-        layer: L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'),
-        legend: '<i style="background: black; opacity: 0.5"></i><p><b>legend 1</b></p>'
+    layer4: {
+        layer: L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_only_labels/{z}/{x}/{y}{r}.png')
+    },
+    layer3: {
+        layer: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}')
     },
     layer2: {
         layer: L.geoJson.ajax('https://raw.githubusercontent.com/jasparkatt/Storymaps/master/data/SpotX.geojson', {
             pointToLayer: function(feature, latlng) {
-                return L.circleMarker(latlng, geojsonMarkerOptions);
+                return L.circleMarker(latlng, geojsonMarkerOptions).bindTooltip(feature.properties.Stream, { className: 'label_tooltip' });
             }
-        }),
-        legend: '<i style="background: orange; opacity: 0.5"></i><p><b>legend 2</b></p>'
+        })
     },
-    layer3: {
-        layer: L.tileLayer('http://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png')
+    layer1: {
+        layer: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Shaded_Relief/MapServer/tile/{z}/{y}/{x}')
     }
 };
 var scenes = {
-    scene1: { lat: 44, lng: -89.00, zoom: 7, layers: [layers.layer2], name: "scene 1" },
-    scene2: { lat: 44.5701158, lng: -89.00, zoom: 10, layers: [layers.layer2], name: "scene 2" },
-    scene3: { lat: 45.111235, lng: -89.00, zoom: 12, layers: [layers.layer1, layers.layer2], name: "scene 3" }
+    scene1: { lat: 44, lng: -91.00, zoom: 7, layers: [layers.layer2], name: "scene 1" },
+    scene2: { lat: 44.05, lng: -89.6, zoom: 10, layers: [layers.layer4, layers.layer1, layers.layer2], name: "scene 2" },
+    scene3: { lat: 43.5, lng: -90.95, zoom: 9.3, layers: [layers.layer1, layers.layer2], name: "scene 3" }
 };
-$('#storymap').storymap({
-    scenes: scenes,
-    baselayer: layers.layer3,
-    legend: true,
-    loader: true,
-    flyto: true,
-    credits: "Build with <i class='material-icons' style='color: red; font-size: 10px;'>favorite</i> from Bo Zhao",
-    scalebar: true,
-    scrolldown: true,
-    progressline: true,
-    navwidget: true,
-    createMap: function() {
-        var map = L.map($(".storymap-map")[0], { zoomControl: false }).setView([44, -120], 7);
-        basemap = this.baselayer.layer.addTo(map);
-        return map;
-    }
-});
