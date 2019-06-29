e0128e1092f61a60e42a8140093ba9816a0c9833
unknown <you@example.com>
2019-06-29 15:24:57 -0500

    plodding along 2

--- scripts/main.js
+++ scripts/main.js
@@ -21,9 +21,9 @@ var layers = {
             style:CntyBndOptions
         })
      },
-   /*  layer4: {
+    layer4: {
         layer: L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_only_labels/{z}/{x}/{y}{r}.png')
-    }, */
+    },
     layer3: {
         layer: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}')
     },
@@ -40,6 +40,6 @@ var layers = {
 };
 var scenes = {
     scene1: { lat: 44, lng: -91.00, zoom: 7, layers: [layers.layer2], name: "scene 1" },
-    scene2: { lat: 44.05, lng: -89.6, zoom: 10, layers: [ layers.layer1, layers.layer2, layers.layer5], name: "scene 2" },
-    scene3: { lat: 43.5, lng: -90.95, zoom: 9.3, layers: [layers.layer1, layers.layer2], name: "scene 3" }
+    scene2: { lat: 44.05, lng: -89.6, zoom: 10, layers: [ layers.layer1, layers.layer2, layers.layer5, layers.layer4], name: "scene 2" },
+    scene3: { lat: 43.5, lng: -90.95, zoom: 9.3, layers: [layers.layer1, layers.layer2, layers.layer5, layers.layer4], name: "scene 3" }
 };
