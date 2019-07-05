3e81a17e1eae90241f3019bf502fc1b2e2a7b41f
jaspar katt <jasparkatt@yahoo.com>
2019-07-05 13:35:36 -0500

    add some gulp stuff

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,52 @@ const {src, dest, watch, lastRun, series, parallel} = require('gulp');
 const imagemin = require('gulp-imagemin');
 const plumber = require('gulp-plumber');
 const cleanCss = require('gulp-clean-css');
-const htmlmin = require('gulp=htmlmin');
+const htmlmin = require('gulp-htmlmin');
+const uglify = require('gulp-uglify');
 
 
 
 //construct some paths
 const files = {
+    htmlPath: './index.html',
+    cssPath: './style/*.css',
+    jsPath: './scripts/*.js',
     imgPath: './img/*',
     imgBuild: './build/img/',
+    htmlBuild: './build/',
+    cssBuild: './build/style/',
+    jsBuild: './build/scripts/'
     // imgTemp: './temp/img/'
 }
+
 function imgTask() {
     return src(files.imgPath, { since: lastRun(imgTask) })
                 .pipe(plumber())
                 .pipe(imagemin())
                 .pipe(dest(files.imgBuild))
                 // .pipe(dest(files.imgTemp))
+}
+
+function cssTask() {
+    return src(files.cssPath, {since: lastRun(cssTask)})
+        .pipe(plumber())
+        .pipe(cleanCss())
+        .pipe(dest(files.cssBuild))
+}
+
+function jsTask() {
+    return src(files.jsPath, {since: lastRun(jsTask)})
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(dest(files.jsBuild))
+}
+
+function htmlTask() {
+    return src(files.htmlPath, {since: lastRun(htmlTask)})
+        .pipe(plumber())
+        .pipe(htmlmin({removeComments: true}))
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(dest(files.htmlBuild))
 }
 
 //create some watchers
@@ -29,3 +59,13 @@ function watchImg(){
 };
 //export tasks
 exports.imgTask = imgTask;
+exports.cssTask = cssTask;
+exports.htmlTask = htmlTask;
+exports.jsTask = jsTask;
+
+exports.rebuild = series(
+    imgTask,
+    htmlTask,
+    cssTask,
+    jsTask
+);
