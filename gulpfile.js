00e602b5fe8517a538d3305cf69e8ea9875512ea
unknown <you@example.com>
2019-07-04 19:52:28 -0500

    add gulp fle

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+//load plugins
+
+const {src, dest, watch, lastRun, series, parallel} = require('gulp');
+const imagemin = require('gulp-imagemin');
+const plumber = require('gulp-plumber');
+
+//construct some paths
+const files = {
+    imgPath: './img/*',
+    imgBuild: './build/img/',
+    // imgTemp: './temp/img/'
+}
+function imgTask() {
+    return src(files.imgPath, { since: lastRun(imgTask) })
+                .pipe(plumber())
+                .pipe(imagemin())
+                .pipe(dest(files.imgBuild))
+                // .pipe(dest(files.imgTemp))
+}
+
+//create some watchers
+function watchImg(){
+    watch('./img/', imgTask);
+    console.log('running imgTask...did you add some new pics?');
+};
+//export tasks
+exports.imgTask = imgTask;
