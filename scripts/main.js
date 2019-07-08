569aec9993beb4d721a78ef021bcdc697907182b
jaspar katt <jasparkatt@yahoo.com>
2019-07-07 22:23:48 -0500

    some css changes

--- scripts/main.js
+++ scripts/main.js
@@ -8,11 +8,11 @@ var geojsonMarkerOptions = {
 };
 
 var classOne = {
-    "color": "#7380D8",
-    "weight": 1.5,
-    "opacity": 0.8,
-    "dashArray": '1',
-    "fillOpacity": 0.6
+    color: "#7380D8",
+    weight: 1.5,
+    opacity: 0.8,
+    dashArray: '1',
+    fillOpacity: 0.6
 };
 
 
@@ -54,8 +54,8 @@ var classOne = {
         overview: {lat: 44.0000000, lng: -123.5000000, zoom: 7, name: 'Cover Page', layers: []},
         centralsands: {lat: 44.09, lng: -89.6, zoom: 9.5, name: 'The Central Sands', layers: [layers.counties]},
         wautoma: {lat: 44.119, lng: -89.45, zoom: 10.5, name: 'Linch Pin Of The Sands', layers: [layers.satellite]},
-        eugene: {lat: 44.0549563, lng: -123.0958048, zoom: 13, name: 'Eugene', layers: []},
-        salem: {lat: 44.09419055, lng: -89.60356407, zoom: 9, name: 'Salem', layers: []},
+        westfork: {lat: 43.5, lng: -90.95, zoom: 10, name: 'Avalanche', layers: [layers.counties]},
+        salem: {lat: 44.09419055, lng: -89.60356407, zoom: 9, name: 'Salem', layers: [layers.counties]},
         driftless: {lat: 43.5, lng: -90.95, zoom: 9.3, name: 'The Driftless Area', layers: [layers.ESRI, layers.counties]},
         wisconsin: {lat: 44.7, lng: -90.95, zoom: 7, name: 'Wisconsin State', layers: [layers.class1, layers.counties]},
         end: {lat: 44.0000000, lng: -123.5000000, zoom: 7, name: 'The End'}
